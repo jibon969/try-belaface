018ea583a24a0cf7cd92c0abb72c6d2b2571ee42
jayed969 <jayed.swe@gmail.com>
2023-02-10 23:44:29 +0600

    remove console log

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  console.log("Hello, World !");
   return (
     <View style={styles.container}>
       <Text>Hello, React Native</Text>
